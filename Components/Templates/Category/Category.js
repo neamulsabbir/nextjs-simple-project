9e5d9c170d55d1bd50350c14afaa56486148f4fa
Neamul Kabri Sabbir <neamulkabir97@gmail.com>
2024-03-27 23:54:45 +0600

    added css

--- Components/Templates/Category/Category.js
+++ Components/Templates/Category/Category.js
@@ -20,7 +20,11 @@ const Category = () => {
 	return (
 		<section className="w-11/12 lg:w-10/12 max-w-7xl mx-auto py-0 lg:py-10 lg:flex justify-between items-start ">
 			<div className="w-full flex items-center justify-between lg:block lg:w-2/12 my-10 lg:my-0 lg:mt-4 static">
-				<Link scroll={false} href="/category/all">
+				<Link
+					scroll={false}
+					href="/category/all"
+					className={`${params.category === category && "text-green-400"} `}
+				>
 					All
 				</Link>
 				{[...uniqueCategories].map((category) => (
