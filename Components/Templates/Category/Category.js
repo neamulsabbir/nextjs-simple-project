78c1360b67007d992dab59e3c7f4b3d1d4ae6fc9
Neamul Kabri Sabbir <neamulkabir97@gmail.com>
2024-03-27 23:06:30 +0600

    some issue fixed

--- Components/Templates/Category/Category.js
+++ Components/Templates/Category/Category.js
@@ -1,7 +1,6 @@
 "use client";
 import ProductCard from "@/Components/SharedFolder/ProductCard";
-import { categories } from "@/Components/SharedFolder/constant";
-import { getProductDataByCategory } from "@/data/getData";
+import { getProductData, getProductDataByCategory } from "@/data/getData";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
@@ -10,34 +9,38 @@ const Category = () => {
 
 	const data = getProductDataByCategory(params.category || "all");
 
+	const alldata = getProductData();
+
+	const uniqueCategories = new Set();
+
+	alldata.forEach((category) => {
+		uniqueCategories.add(category.category);
+	});
+
 	return (
 		<section className="w-11/12 lg:w-10/12 max-w-7xl mx-auto py-0 lg:py-10 lg:flex justify-between items-start ">
 			<div className="w-full flex items-center justify-between lg:block lg:w-2/12 my-10 lg:my-0 lg:mt-4 static">
-				{categories.map((category) => (
+				<Link scroll={false} href="/category/all">
+					All
+				</Link>
+				{[...uniqueCategories].map((category) => (
 					<Link
-						href={`/category/${category.category}`}
-						key={category.id}
-						className={` mt-4 ${
-							params.category === category.category && "text-green-400"
-						} block h-6 box-border`}
+						scroll={false}
+						href={`/category/${category}`}
+						key={category}
+						className={` mt-4 ${params.category === category && "text-green-400"} block h-6 box-border`}
 					>
-						<span className="hover:border-b border-black">{category.name}</span>
+						<span className="hover:border-b border-black">{category}</span>
 					</Link>
 				))}
 			</div>
 
 			<div className="w-full lg:w-10/12  my-4 lg:my-10 ">
-				{data?.length < 1 ? (
-					<div className="w-full my-20  ">
-						<h2 className="text-2xl text-gray-600 text-center">No Products Available</h2>
-					</div>
-				) : (
-					<div className="grid grid-cols-2 gap-4 lg:grid-cols-3">
-						{data?.map((product) => (
-							<ProductCard key={product.id} product={product} />
-						))}
-					</div>
-				)}
+				<div className="grid grid-cols-2 gap-4 lg:grid-cols-3">
+					{data?.map((product) => (
+						<ProductCard key={product.id} product={product} />
+					))}
+				</div>
 			</div>
 		</section>
 	);
