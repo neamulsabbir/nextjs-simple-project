16e5e5dde92d9375a8cfb547d792a18ac93ca8c7
Neamul Kabri Sabbir <neamulkabir97@gmail.com>
2024-03-27 23:10:46 +0600

    svg added

--- Components/SharedFolder/Navbar.js
+++ Components/SharedFolder/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
 		<nav className="flex items-center justify-between w-11/12 lg:w-10/12 max-w-7xl py-5 lg:py-6 mx-auto">
 			<div className="flex gap-5 items-center justify-between lg:w-8/12 text-[#1A1A1A]">
 				<div className="flex items-center gap-2">
-					<Image src="/images/svg/svg/menu.svg" width={20} height={20} className="lg:hidden " alt="" />
+					<Image src="/images/svg/menu.svg" width={20} height={20} className="lg:hidden " alt="" />
 					<Link href="/">
 						<Image src="/images/lws-logo-black.svg" height={120} width={120} alt="" />
 					</Link>
